a151d4add00a9c2c3eb78d7659b5b828f5f3bab4
Skeeter <qwongo@gmail.com>
2019-11-09 06:03:26 -0500

    LMAOOOOOOO

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -1,12 +1,22 @@
 extends KinematicBody2D
 #This script controls movement
+const UP = Vector2(0,-1)
 var motion = Vector2()
+export var speed = 650
+export var gravity = 5
+export var jump_force = -400
 
 func _physics_process(delta):
+	motion.y += gravity
 	if Input. is_action_pressed("ui_right"):
-		motion.x = 100
+		motion.x = speed
 	elif Input. is_action_pressed("ui_left"):
-		motion.x = -100
+		motion.x = -speed
 	else:
 		motion.x = 0
-	move_and_slide(motion)
+		
+		
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_up"):
+			motion.y = jump_force
+	motion = move_and_slide(motion, UP)
